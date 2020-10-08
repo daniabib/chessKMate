4fc8bc84fda04ac4f3c5d082c7392be261b68056
Daniel Abib <danielsabib@gmail.com>
2020-10-08 16:58:00 -0300

    Immutability enhancement to Tile class

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # chessKMate
-# chessKMate
-# chessKMate
-# chessKMate
+
---
+++ src/com/chess/engine/board/Tile.java
@@ -0,0 +1,73 @@
+package com.chess.engine.board;
+
+import com.chess.engine.pieces.Piece;
+import spals.shaded.com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+
+    protected final int tileCoordinate;
+
+    private static final Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+
+        for (int i = 0; i < 64; i++) {
+            emptyTileMap.put(i, new EmptyTile(i));
+        }
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile {
+
+        EmptyTile(int coordinate) {
+            // super calls the constructor of the superclass.
+            super(coordinate);
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int coordinate, Piece pieceOnTile) {
+            super(coordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+}
